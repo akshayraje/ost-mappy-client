90adb18980c495812f4414f67ae72d66263322b7
Shraddha <shraddha.patil@ost.com>
2019-02-21 20:42:38 +0530

    constants file

---
+++ src/constants.js
@@ -0,0 +1,39 @@
+const dataMap = {
+    listItem1: {
+      data_defination: "Transfer",
+      data_defination_version: "1.0.0",
+      rule_name: "ok",
+      rule_method_name: "ok",
+      rule_parameters: [],
+      token_id: 100
+    },
+    listItem2: {
+    data_defination: "Send",
+    data_defination_version: "1.2.0",
+    rule_name: "ok2",
+    rule_method_name: "ok2",
+    rule_parameters: [],
+    token_id: 200
+  },
+  listItem3: {
+    data_defination: "Buy",
+    data_defination_version: "1.3.0",
+    rule_name: "ok3",
+    rule_method_name: "ok3",
+    rule_parameters: [],
+    token_id: 300
+  },
+  listItem4: {
+    data_defination: "OK",
+    data_defination_version: "1.4.0",
+    rule_name: "ok4",
+    rule_method_name: "ok4",
+    rule_parameters: [],
+    token_id: 400
+  }
+}
+
+
+
+
+export default dataMap;
