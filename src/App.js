4c0da885e30db0a731e50f23e76ad4d0278632d1
Preshita Shirke <preshita@ost.com>
2019-02-21 17:25:01 +0530

    code changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
 import Card from './components/Card'
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      error : null,
+      isLoaded : false,
+      users : []
+    };
+  }
+
+  componentDidMount(){
+    axios.get('http://172.16.0.223:4040/api/users')
+      .then(res => {
+        const users = res.data['users'];
+        this.setState({
+          isLoaded : true,
+          users
+        })
+      })
+      .catch(err => {
+        this.setState({
+          error : err,
+          isLoaded : true,
+        })
+      })
+  }
+
   render() {
-    return (
-      <div className="app">
-        <div className="row">
-          <div className="col-md-4">
-            <Card title="My card" text="My card's text" buttonText="Click me!"/>
-          </div>
-          <div className="col-md-4">
-            <Card title="My card" text="My card's text" buttonText="Click me!"/>
-          </div>
-          <div className="col-md-4">
-            <Card title="My card" text="My card's text" buttonText="Click me!"/>
-          </div>
+    if (this.state.error) {
+      return <div>Error: {this.state.error.message}</div>;
+    } else if (!this.state.isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="row p-4">
+              {this.state.users.map(user => (
+                 <Card key={user._id} user={user}/>
+              ))}
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import { toSvg } from 'jdenticon';
 
 class Card extends Component {
     render() {
+      const user = this.props.user,
+            colClass = this.props.colClass ? this.props.colClass : 'col-md-3';
         return (
+          <div className={colClass+' py-2'}>
             <div className="card">
-                <div className="card-body">
-                    <h5 className="card-title">{this.props.title}</h5>
-                    <p className="card-text">{this.props.text}</p>
-                    <a href="#" className="btn btn-primary">{this.props.buttonText}</a>
-                </div>
+              <svg className="card-img-top" data-jdenticon-value={user._id}></svg>
+              <div className="card-body">
+                <h5 className="card-title">{user.username}</h5>
+                <p className="card-text">{user.mobile_number}</p>
+                <p className="card-text">{user.description}</p>
+              </div>
             </div>
+          </div>
         );
     }
 }
