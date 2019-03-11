ca09d02ed654da02c512fa7a1bd57a78008ef4c6
Preshita Shirke <preshita@ost.com>
2019-03-11 18:37:38 +0530

    minor changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import jdenticon from "jdenticon";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import jdenticon from 'jdenticon';
 
 class Card extends Component {
   constructor(props) {
     super(props);
-    this.colClass = this.props.colClass || "col-lg-3 col-sm-6";
+    this.colClass = this.props.colClass || 'col-lg-3 col-sm-6';
   }
 
   componentDidMount() {
@@ -18,12 +18,23 @@ class Card extends Component {
         <div className="card shadow">
           <svg className="card-img-top" data-jdenticon-value={this.props.user._id} />
           <div className="card-body">
-            <h5 className="card-title">{this.props.user.user_display_name || this.props.user.username}</h5>
+            <h5 className="card-title text-truncate">
+              {this.props.user.user_display_name || this.props.user.username}
+            </h5>
             <p className="card-text">Phone: {this.props.user.mobile_number}</p>
             {this.props.user.token_holder_address ? (
-              <Link className="card-link" to={`/user/${this.props.user._id}`}>
-                Get QR Codes
-              </Link>
+              <div className="row">
+                <div className="col-6">
+                  <Link className="card-link" to={`/user/${this.props.user._id}/tx`}>
+                    Transaction QR Codes
+                  </Link>
+                </div>
+                <div className="col-6">
+                  <Link className="card-link" to={`/user/${this.props.user._id}/devices`}>
+                    Devices QR Codes
+                  </Link>
+                </div>
+              </div>
             ) : (
               <span className="text-black-50">User not setup</span>
             )}
